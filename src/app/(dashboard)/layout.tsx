8c22fed1cd2b68c54e00ce6d3c9d8d957e8a11e6
DESKTOP-VO6GBFM\Lyu <aitl12@etribe.co.kr>
2026-04-20 17:55:04 +0900

    style: remove global header entirely - content only layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,4 @@
 import Sidebar from "@/components/layout/sidebar";
-import Header from "@/components/layout/header";
 
 export default function DashboardLayout({
   children,
@@ -9,10 +8,9 @@ export default function DashboardLayout({
   return (
     <div className="flex min-h-screen">
       <Sidebar />
-      <div className="flex flex-1 flex-col pl-64">
-        <Header />
-        <main className="flex-1 p-6">{children}</main>
-      </div>
+      <main className="flex-1 p-6 pl-70">
+        {children}
+      </main>
     </div>
   );
 }
